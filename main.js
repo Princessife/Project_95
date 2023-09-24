a3a5235a9e1ce8c644d323f786930a8ce9aff143
Princessife <145937164+Princessife@users.noreply.github.com>
2023-09-24 15:55:47 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -114,7 +114,7 @@ function my_keydown(e)
 		if(ball_y >=1050)
 		{
 			
-			ball_x = ball_yx- block_image_width;
+			ball_x = ball_x + block_image_width;
 			console.log("block image width = " + block_image_width);
 			console.log("When Right arrow key is pressed, X =  " + ball_x + " , Y = "+ball_y);
 			canvas.remove(ball_obj);
